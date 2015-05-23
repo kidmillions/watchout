c0025fb47b8a330e8c0191d9fe6e5fa12ffef5b9
kidmillions <claymanchris@gmail.com>
2015-05-22 18:25:41 -0700

    Made Player draggable

--- watchout.js
+++ watchout.js
@@ -8,6 +8,7 @@ var svg = d3.selectAll("body").append("svg")
   .attr("width", width)
   .attr("height", height);
 
+
 var render = function(data){
   var asteroids = svg.selectAll("image").data(data, function(d) {return d.id;});
 
@@ -20,35 +21,28 @@ var render = function(data){
   asteroids.transition().duration(900)
   .attr("x", function(d) {return d.x})
   .attr("y", function(d) {return d.y});
-
   asteroids.exit()
     .remove();
 };
 
+var renderPlayer = function(){
+  var player = svg
+    .append('circle')
+      .attr('class', 'circle')
+      .attr("r", 10)
+      .attr('cx', (width/2))
+      .attr('cy', (height/2));
 
+  var moveCircle = function(){
+    d3.select(this)
+    .attr("cx", d3.event.x)
+    .attr("cy",d3.event.y);
+  };
 
-// var asteroid = svg.selectAll("image").data(data)
-//   .attr("src", "./asteroid.png");
+  var drag = d3.behavior.drag().on("drag", moveCircle);
 
-// asteroid.enter()
-//   .append("image")
-//   .attr("x", function(d) {return d.x})
-//   .attr("y", function(d) {return d.y});
-
-
-// //what kinds of data are important to each asteroid?
-// //what is singular to each asteroid?
-//   //its position within the svg
-//     //which is randomly generated upon updating
-
-// var randomLocation = function() {
-//   var x = Math.random() * width;
-//   var y = Math.random() * height;
-//   return {x: x, y: y};
-// };
-
-// var data = randomLocation();
-
+  player.call(drag);
+}
 var makeAsteroids = function() {
   return _.range(0, nAsteroids).map(function(index) {
     return {
@@ -58,6 +52,8 @@ var makeAsteroids = function() {
     };
   });
 };
+
+renderPlayer();
 
 var play = function() {
   var turn = function() {
